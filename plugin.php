b1ca9237e4ae4167d68ca67079b9f9067b251a45
Arseniy Olevskiy <77734966+arolus@users.noreply.github.com>
2023-06-10 17:20:26 +0300

    Regular commit

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Number Generator by Calculator.iO
+Plugin URI: https://www.calculator.io/number-generator/
+Description: The random number generator provides a set of random numbers according to user-specified options such as range, repeat, and sort.
+Version: 1.0.0
+Author: Calculator.io
+Author URI: https://www.calculator.io/
+License: GPLv2 or later
+Text Domain: ci_number_generator
+*/
+
+if (!function_exists('add_shortcode')) die("No direct call");
+
+function display_ci_number_generator(){
+    $page = 'index.html';
+    return '<h2><a href="https://www.calculator.io/number-generator/" target="_blank"><img src="' . plugins_url('assets/images/icon-48.png', __FILE__ ) . '" width="48" height="48"></a> Number Generator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . plugins_url($page, __FILE__ ) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_number_generator_iframe"></iframe></div>';
+}
+
+add_shortcode( 'ci_number_generator', 'display_ci_number_generator' );
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Number Generator by Calculator.iO ===
+Contributors: calculatorio
+Tags: 
+Requires at least: 5.0
+Tested up to: 6.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+The random number generator provides a set of random numbers according to user-specified options such as range, repeat, and sort.
+
+== Description ==
+
+The random number generator provides a set of random numbers according to user-specified options such as range, repeat, and sort.
+
+== Installation ==
+
+1. Upload the /ci_number_generator/ folder to the /wp-content/plugins/ directory.
+2. Activate the [Number Generator](https://www.calculator.io/number-generator/ "Number Generator Homepage") plugin through the "Plugins" menu in WordPress.
+3. Configure your settings via Appearance > Widgets > Number Generator.
+4. And you're good to go!
+
+== Screenshots ==
+1. The Number Generator Input Form.
+2. The Number Generator Calculation Results.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
